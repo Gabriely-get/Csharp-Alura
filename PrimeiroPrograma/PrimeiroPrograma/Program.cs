80f477e176c6233a12124a4cbbc2b3a2da19d42b
unknown <gabrielysantos.informatica@gmail.com>
2024-01-22 09:52:03 -0300

    Add aula 01 - cosole write

---
+++ PrimeiroPrograma/PrimeiroPrograma/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Hello, World!");
+
+string mensagemDeBoasVindas = "Oi, bem vindo ao C#";
+string nome = "Gabriely";
+string sobrenome = "Cristovão";
+
+Console.WriteLine(mensagemDeBoasVindas + ", "+ nome +" "+ sobrenome);
